7e0ec34eaa84fe0a37868efd19466c98c12f5e65
PKui <1321095399@qq.com>
2022-07-24 22:22:46 +0800

    feat vue 双向绑定-完成数据劫持和双大括号展示

---
+++ pages/vue/interactive-data-binding/index.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import CLayout from "../../../layout";
+
+import styles from "./index.module.less";
+
+function InteractiveDataBinding() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>vue 双向绑定的简单实现</div>
+      <iframe
+        src="/iframe/vue/interactive-data-binding/index.html"
+        className={styles.frame}
+      ></iframe>
+    </div>
+  );
+}
+
+InteractiveDataBinding.getLayout = function getLayout(page: ReactNode) {
+  return <CLayout>{page}</CLayout>;
+};
+
+export default InteractiveDataBinding;
